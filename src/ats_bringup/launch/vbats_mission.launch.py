91758a2a1332b2478fd0c4e25fe5af635ce2038a
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2026-02-17 14:36:22 +0100

    Redid gains for next test

--- src/ats_bringup/launch/vbats_mission.launch.py
+++ src/ats_bringup/launch/vbats_mission.launch.py
@@ -75,14 +75,14 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {'frequency': 100.},
-            {'Kp_linear': 30.0},
-            {'Ki_linear': 6.0},
+            {'Kp_linear': 35.0},
+            {'Ki_linear': 4.0},
             {'Kd_linear': 2.0},
             {'Kp_angular': 0.75},
             {'Ki_angular': 0.0},
             {'Kd_angular': 0.45},
             {'alpha': 0.1},
-            {'windup_clip': 0.15},
+            {'windup_clip': 0.1},
             {'altitude_anchoring': True},
             {'test_execution_time': False}
         ],
